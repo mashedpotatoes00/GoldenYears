61d88a4ae92d66a83d56e2d376652e16711b7f42
Will <wleonar1@unb.ca>
2018-11-07 20:44:33 -0400

    good start

--- app/src/main/java/com/gold/goldenyears/MainActivity.java
+++ app/src/main/java/com/gold/goldenyears/MainActivity.java
@@ -1,13 +1,51 @@
 package com.gold.goldenyears;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView.OnNavigationItemSelectedListener navSelectListener = new NavigationView.OnNavigationItemSelectedListener() {
+        public boolean onNavigationItemSelected(MenuItem menuItem) {
+            menuItem.setChecked(true);
+            drawerLayout.closeDrawers();
+            //put actions on press here (fragments)
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar appToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(appToolBar);
+        ActionBar appActionBar = getSupportActionBar();
+        appActionBar.setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_navigation_drawer, R.string.close_navigation_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(navSelectListener);
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (toggle.onOptionsItemSelected(menuItem)) {
+            return true;
+        }
+        return false;
     }
 }
