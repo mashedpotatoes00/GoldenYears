a7ac85116311698d1f781d470ae79db71d9ce1e0
Avery Briggs <abriggs914@gmail.com>
2018-11-13 01:32:49 -0400

    2018-11-13 Added input fields for user name and age. First use message is still not working correctly.

--- app/src/main/java/com/gold/goldenyears/MainActivity.java
+++ app/src/main/java/com/gold/goldenyears/MainActivity.java
@@ -1,22 +1,31 @@
 package com.gold.goldenyears;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v4.widget.DrawerLayout;
 import android.support.design.widget.NavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
     private NavigationView.OnNavigationItemSelectedListener navSelectListener = new NavigationView.OnNavigationItemSelectedListener() {
         public boolean onNavigationItemSelected(MenuItem menuItem) {
 
@@ -52,11 +61,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart", true);
 
+        if (firstStart) {
+            showStartDialog();
+        }
         getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new Home()).commit();
 
         ActionBar appActionBar = getSupportActionBar();
-        appActionBar.setDisplayHomeAsUpEnabled(true);
+        if (null != appActionBar) {
+            appActionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         drawerLayout = findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_navigation_drawer, R.string.close_navigation_drawer);
@@ -65,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(navSelectListener);
-
+        radioGroup = findViewById(R.id.radioGroup);
     }
 
     public boolean onOptionsItemSelected(MenuItem menuItem) {
@@ -73,6 +89,33 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    private void showStartDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Welcome")
+                .setMessage("This should only be shown once")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create().show();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
+    }
+
+    public void checkButton(View v) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+
+        radioButton = findViewById(radioId);
+
+//        Toast.makeText(this, "Selected Radio Button: " + radioButton.getText(),
+//                Toast.LENGTH_SHORT).show();
     }
 }
 
